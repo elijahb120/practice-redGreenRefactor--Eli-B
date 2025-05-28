d02764b1b7dcc771da12465c52d5091481d3e0f6
JoshuaYoshi1922 <joshua.yoshi1922@gmail.com>
2025-05-27 20:22:52 -0500

    Add files via upload

---
+++ redgreen.js
@@ -0,0 +1,13 @@
+function countVowels (str,count) {
+    if (str.includes("a", "e", "i", "o", "u"));
+        for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) {
+      count++;
+    }
+  }
+  return count;
+}
+
+module.exports = {
+    countVowels
+}
